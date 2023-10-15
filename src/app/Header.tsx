585ef768a86a4184c3442642a4ea2f37a6065228
Milka Fernando Pulle <milkafernandopulle@gmail.com>
2023-10-15 11:15:23 +0100

    added more shadcn and login form

---
+++ src/app/Header.tsx
@@ -0,0 +1,332 @@
+"use client";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import clsx from "clsx";
+import { Fragment, useState } from "react";
+import { Dialog, Popover, Tab, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  QuestionMarkCircleIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+const currencies = ["CAD", "USD", "AUD", "EUR", "GBP"];
+const navigation = {
+  categories: [
+    {
+      name: "Women",
+      featured: [
+        {
+          name: "New Arrivals",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
+          imageAlt: "Models sitting back to back, wearing Basic Tee in black and bone.",
+        },
+        {
+          name: "Basic Tees",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
+          imageAlt:
+            "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
+        },
+        {
+          name: "Accessories",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-category-03.jpg",
+          imageAlt: "Model wearing minimalist watch with black wristband and white watch face.",
+        },
+        {
+          name: "Carry",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-category-04.jpg",
+          imageAlt:
+            "Model opening tan leather long wallet with credit card pockets and cash pouch.",
+        },
+      ],
+    },
+    {
+      name: "Men",
+      featured: [
+        {
+          name: "New Arrivals",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-01.jpg",
+          imageAlt:
+            "Hats and sweaters on wood shelves next to various colors of t-shirts on hangers.",
+        },
+        {
+          name: "Basic Tees",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-02.jpg",
+          imageAlt: "Model wearing light heather gray t-shirt.",
+        },
+        {
+          name: "Accessories",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-03.jpg",
+          imageAlt:
+            "Grey 6-panel baseball hat with black brim, black mountain graphic on front, and light heather gray body.",
+        },
+        {
+          name: "Carry",
+          href: "#",
+          imageSrc: "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-04.jpg",
+          imageAlt:
+            "Model putting folded cash into slim card holder olive leather wallet with hand stitching.",
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: "Company", href: "#" },
+    { name: "Stores", href: "#" },
+  ],
+};
+const collections = [
+  {
+    name: "Women's",
+    href: "#",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-04-collection-01.jpg",
+    imageAlt: "Woman wearing a comfortable cotton t-shirt.",
+  },
+  {
+    name: "Men's",
+    href: "#",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-04-collection-02.jpg",
+    imageAlt: "Man wearing a comfortable and casual cotton t-shirt.",
+  },
+  {
+    name: "Desk Accessories",
+    href: "#",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg",
+    imageAlt: "Person sitting at a wooden desk with paper note organizer, pencil and tablet.",
+  },
+];
+const trendingProducts = [
+  {
+    id: 1,
+    name: "Leather Long Wallet",
+    color: "Natural",
+    price: "$75",
+    href: "#",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg",
+    imageAlt: "Hand stitched, orange leather long wallet.",
+  },
+  // More products...
+];
+const perks = [
+  {
+    name: "Free returns",
+    imageUrl: "https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg",
+    description:
+      "Not what you expected? Place it back in the parcel and attach the pre-paid postage stamp.",
+  },
+  {
+    name: "Same day delivery",
+    imageUrl: "https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg",
+    description:
+      "We offer a delivery service that has never been done before. Checkout today and receive your products within hours.",
+  },
+  {
+    name: "All year discount",
+    imageUrl: "https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg",
+    description:
+      'Looking for a deal? You can use the code "ALLYEAR" at checkout and get money off all year round.',
+  },
+  {
+    name: "For the planet",
+    imageUrl: "https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg",
+    description:
+      "We’ve pledged 1% of sales to the preservation and restoration of the natural environment.",
+  },
+];
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="sticky top-0 shadow">
+      <nav aria-label="Top">
+        {/* Secondary navigation */}
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              {/* Logo (lg+) */}
+              <div className="hidden lg:flex lg:flex-1 lg:items-center">
+                <a href="#">
+                  <span className="sr-only">Your Company</span>
+                  <img className="h-8 w-auto" src="/logo.svg" alt="" />
+                </a>
+              </div>
+
+              <div className="hidden h-full lg:flex">
+                {/* Flyout menus */}
+                <Popover.Group className="inset-x-0 bottom-0 px-4">
+                  <div className="flex h-full justify-center space-x-8">
+                    {navigation.categories.map((category) => (
+                      <Popover key={category.name} className="flex">
+                        {({ open }) => (
+                          <>
+                            <div className="relative flex">
+                              <Popover.Button
+                                className={clsx(
+                                  open ? "text-indigo-600" : "text-gray-700 hover:text-gray-800",
+                                  "relative flex items-center justify-center text-sm font-medium transition-colors duration-200 ease-out"
+                                )}>
+                                {category.name}
+                                <span
+                                  className={clsx(
+                                    open ? "bg-indigo-600" : "",
+                                    "absolute inset-x-0 -bottom-px z-20 h-0.5 transition duration-200 ease-out"
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              </Popover.Button>
+                            </div>
+
+                            <Transition
+                              as={Fragment}
+                              enter="transition ease-out duration-200"
+                              enterFrom="opacity-0"
+                              enterTo="opacity-100"
+                              leave="transition ease-in duration-150"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0">
+                              <Popover.Panel className="absolute inset-x-0 top-full z-10 bg-white text-sm text-gray-500">
+                                {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                                <div
+                                  className="absolute inset-0 top-1/2 bg-white shadow"
+                                  aria-hidden="true"
+                                />
+                                {/* Fake border when menu is open */}
+                                <div
+                                  className="absolute inset-0 top-0 mx-auto h-px max-w-7xl px-8"
+                                  aria-hidden="true">
+                                  <div
+                                    className={clsx(
+                                      open ? "bg-gray-200" : "bg-transparent",
+                                      "h-px w-full transition-colors duration-200 ease-out"
+                                    )}
+                                  />
+                                </div>
+
+                                <div className="relative">
+                                  <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                                    <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                                      {category.featured.map((item) => (
+                                        <div key={item.name} className="group relative">
+                                          <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                                            <img
+                                              src={item.imageSrc}
+                                              alt={item.imageAlt}
+                                              className="object-cover object-center"
+                                            />
+                                          </div>
+                                          <a
+                                            href={item.href}
+                                            className="mt-4 block font-medium text-gray-900">
+                                            <span
+                                              className="absolute inset-0 z-10"
+                                              aria-hidden="true"
+                                            />
+                                            {item.name}
+                                          </a>
+                                          <p aria-hidden="true" className="mt-1">
+                                            Shop now
+                                          </p>
+                                        </div>
+                                      ))}
+                                    </div>
+                                  </div>
+                                </div>
+                              </Popover.Panel>
+                            </Transition>
+                          </>
+                        )}
+                      </Popover>
+                    ))}
+
+                    {navigation.pages.map((page) => (
+                      <a
+                        key={page.name}
+                        href={page.href}
+                        className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
+                        {page.name}
+                      </a>
+                    ))}
+                  </div>
+                </Popover.Group>
+              </div>
+
+              {/* Mobile menu and search (lg-) */}
+              <div className="flex flex-1 items-center lg:hidden">
+                <button
+                  type="button"
+                  className="-ml-2 rounded-md bg-white p-2 text-gray-400"
+                  onClick={() => setOpen(true)}>
+                  <span className="sr-only">Open menu</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Search */}
+                <a href="#" className="ml-2 p-2 text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">Search</span>
+                  <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              </div>
+
+              {/* Logo (lg-) */}
+              <a href="#" className="lg:hidden">
+                <span className="sr-only">Your Company</span>
+                <img
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                  className="h-8 w-auto"
+                />
+              </a>
+
+              <div className="flex flex-1 items-center justify-end">
+                <a
+                  href="#"
+                  className="hidden text-sm font-medium text-gray-700 hover:text-gray-800 lg:block">
+                  Search
+                </a>
+
+                <div className="flex items-center lg:ml-8">
+                  {/* Help */}
+                  <a href="#" className="p-2 text-gray-400 hover:text-gray-500 lg:hidden">
+                    <span className="sr-only">Login</span>
+                    <QuestionMarkCircleIcon className="h-6 w-6" aria-hidden="true" />
+                  </a>
+                  <a
+                    href="#"
+                    className="hidden text-sm font-medium text-gray-700 hover:text-gray-800 lg:block">
+                    Login
+                  </a>
+
+                  {/* Cart */}
+                  <div className="ml-4 flow-root lg:ml-8">
+                    <a href="#" className="group -m-2 flex items-center p-2">
+                      <ShoppingBagIcon
+                        className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                        0
+                      </span>
+                      <span className="sr-only">items in cart, view bag</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/molecules/forms/LoginForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Enter a valid email",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
+type LoginFormProps = {};
+export default function LoginForm({}: LoginFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your registered email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Enter your password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button size="lg" className="w-full" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
