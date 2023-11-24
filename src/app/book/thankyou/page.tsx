415a942be5e55608412b1e75be4225b88a38fd9f
Milka Fernando Pulle <milkafernandopulle@gmail.com>
2023-11-24 20:09:11 +0000

    updated

--- src/app/book/thankyou/page.tsx
+++ src/app/book/thankyou/page.tsx
@@ -1,10 +1,28 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+
 type PageProps = {};
 export default function Page({}: PageProps) {
   return (
     <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem molestias quae illo voluptatum,
-      sapiente nihil, delectus accusamus temporibus, ad quibusdam expedita. Ad, dolores autem fuga
-      vitae reprehenderit velit labore provident?
+      <div className="container mb-60">
+        <Card className="max-w-xl m-auto px-12 py-6">
+          <h1 className="font-bold text-3xl text-center mb-6">Booking Success</h1>
+          Your booking has been confirmed. We will send you a confirmation email shortly.
+          <br />
+          <br />
+          <br />
+          <div className="space-x-4 text-center">
+            <Link href="/" className={buttonVariants({ variant: "outline" })}>
+              Back to Home
+            </Link>
+            <Link href="/profile" className={buttonVariants({ variant: "outline" })}>
+              View my bookings
+            </Link>
+          </div>
+        </Card>
+      </div>
     </>
   );
 }
